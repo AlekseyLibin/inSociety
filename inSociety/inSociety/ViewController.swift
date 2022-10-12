98801d81f3862bc0e70368ff4a9be0526cd11186
Aleksey Libin <lexa.libin@gmail.com>
2022-10-12 16:06:53 +0200

    SwiftUI implementation

--- inSociety/inSociety/ViewController.swift
+++ inSociety/inSociety/ViewController.swift
@@ -11,10 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+         
         view.backgroundColor = .gray
     }
+}
 
 
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: Context) -> some UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
 }
 
